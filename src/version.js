8e4d62a498b4b592aa3e42bda9c38bc196c55c95
kanda-houtokukai <merveille.op@gmail.com>
2026-07-24 00:09:19 +0900

    deploy

--- src/version.js
+++ src/version.js
@@ -1,3 +1,3 @@
 // リリースごとに手で上げる（Pages反映確認用）
-export const APP_VERSION = "v2.3-b6d";
-export const BUILD_DATE = "2026-07-23";
+export const APP_VERSION = "v2.3-b6e";
+export const BUILD_DATE = "2026-07-24";
